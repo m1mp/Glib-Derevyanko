5870a50f6b1797f03a11ce32137fe26724f7b5f2
m1mp <113708203+m1mp@users.noreply.github.com>
2023-12-25 10:15:32 +0200

    Fabryc.cpp

--- patterns/Fabryc.cpp
+++ patterns/Fabryc.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <string>
 
-// Базовий клас для створення об'єктів
+
 class Product {
 public:
     virtual void info() = 0;
 };
 
-// Конкретний клас об'єкта
+
 class ConcreteProduct : public Product {
 public:
     void info() override {
@@ -15,13 +15,13 @@ public:
     }
 };
 
-// Фабричний клас для створення об'єктів
+
 class Factory {
 public:
     virtual Product* createProduct() = 0;
 };
 
-// Конкретна фабрика для створення конкретного продукту
+
 class ConcreteFactory : public Factory {
 public:
     Product* createProduct() override {
@@ -30,13 +30,12 @@ public:
 };
 
 int main() {
-    // Створення об'єкту фабрики
+
     Factory* factory = new ConcreteFactory();
 
-    // Використання фабрики для створення об'єкту продукту
+
     Product* product = factory->createProduct();
 
-    // Виклик методу info() для об'єкту продукту
     product->info();
 
     delete product;
