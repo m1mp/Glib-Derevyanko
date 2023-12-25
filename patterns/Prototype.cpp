b813c7447971dc02d91f01da9b1050e94b85cb7f
m1mp <113708203+m1mp@users.noreply.github.com>
2023-12-25 10:16:02 +0200

    Prototype.cpp

--- patterns/Prototype.cpp
+++ patterns/Prototype.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 
-// Abstract base class representing the prototype
+
 class Prototype {
 public:
     virtual Prototype* clone() const = 0;
@@ -9,7 +9,6 @@ public:
     virtual ~Prototype() {}
 };
 
-// Concrete prototype class
 class ConcretePrototype : public Prototype {
 private:
     std::string data;
@@ -17,32 +16,27 @@ private:
 public:
     ConcretePrototype(const std::string& data) : data(data) {}
 
-    // Copy constructor for cloning
     ConcretePrototype(const ConcretePrototype& other) : data(other.data) {}
 
-    // Clone method to create a copy of the object
     Prototype* clone() const override {
         return new ConcretePrototype(*this);
     }
 
-    // Print information about the object
     void printInfo() const override {
         std::cout << "ConcretePrototype: " << data << std::endl;
     }
 };
 
 int main() {
-    // Create an instance of the prototype
     ConcretePrototype original("Original Data");
-    
-    // Clone the prototype to create a new object
+
     Prototype* clonedObject = original.clone();
     
-    // Print information about both objects
+
     original.printInfo();
     clonedObject->printInfo();
     
-    // Cleanup
+
     delete clonedObject;
 
     return 0;
